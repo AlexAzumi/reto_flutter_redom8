b2194ef24fc23fcb73f7ed5a3d7fea7b90674e6c
Alejandro Suárez <alejandro-hdez115@outlook.com>
2022-12-14 23:26:40 -0600

    Add login screen

---
+++ lib/home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Inicio')),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Inicio'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart), label: 'Carrito')
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blueGrey[700],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
