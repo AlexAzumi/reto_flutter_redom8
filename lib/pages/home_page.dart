e17b2c2b9b7ad5b83cef3882b2a0c3c4bff860cc
Alejandro Suárez <alejandro-hdez115@outlook.com>
2022-12-20 19:12:26 -0600

    Add loading screen to index

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:reto_flutter_redom8/services/products.dart';
+import 'dart:developer' as developer;
 
 import '../services/auth.dart';
 
@@ -12,12 +13,23 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
+  bool loading = true;
+  List<Item> itemsToShow = List.empty(growable: true);
 
   @override
   void initState() {
     super.initState();
 
-    Products().getHomeProducts();
+    loadItems();
+  }
+
+  Future<void> loadItems() async {
+    List<Item> fetchedItems = await Products().getHomeProducts();
+
+    setState(() {
+      itemsToShow = fetchedItems;
+      loading = false;
+    });
   }
 
   void onItemTapped(int index) {
@@ -30,6 +42,36 @@ class _HomePageState extends State<HomePage> {
     Auth().signOut();
   }
 
+  Widget _mainContainer() {
+    if (loading) {
+      return _loadingScreen();
+    } else {
+      return _productListing();
+    }
+  }
+
+  Widget _loadingScreen() {
+    return Container(
+        height: double.infinity,
+        width: double.infinity,
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            CircularProgressIndicator(),
+            Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: Text('Cargando productos...'),
+            )
+          ],
+        ));
+  }
+
+  Widget _productListing() {
+    return const Text('Products xd');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,6 +81,7 @@ class _HomePageState extends State<HomePage> {
           IconButton(onPressed: onLogoutTapped, icon: const Icon(Icons.logout))
         ],
       ),
+      body: _mainContainer(),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Inicio'),
